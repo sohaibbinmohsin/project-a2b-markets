ff93f93575cda95bb58701771b5851155632c347
Waseem Sabir <68365040+WaseemSabir@users.noreply.github.com>
2021-04-11 22:06:19 +0500

    Add files via upload

--- front-react/src/Components/Results.js
+++ front-react/src/Components/Results.js
@@ -7,16 +7,17 @@ import Navbar from './Navbar.js'
 let searchResults = () =>{
     const arr = [[['k-1.png',3.2,140,'Gloria Jeans'],['k-2.png',4.0,32,'Pak'],['k-3.png',4.0,33,'Engl'],['k-4.png',4.0,76,'Pak']],[['k-5.png',5.0,89,'kal'],['k-6.png',2.0,5,'hjash']]]
     const [rst,setRst] = react.useState(arr)
-    const [top,setTop] = react.useState("Cakes And Desserts")
-    const [fst,setFst] = react.useState(false) // have to set to true
+    const [name,setName] = react.useState("Cakes And Desserts")
+    const [fst,setFst] = react.useState(true) // have to set to true
+    const [cvr,setCvr] = react.useState('')
+    const [id,setID] = react.useState('6072f45f7f68a634a500373c')
 
     if(fst)
     {
-        const data = {}
-        axios.post("http://localhost:3000/products",data) // api call, url to be chnaged
+        axios.get("http://localhost:8000/markets/"+id) // api call, url to be chnaged
         .then(res => {
-            setRst(res.data) // data needs to be rendered to be in right format as arr
-            setFst(false)
+            setCvr(res.data.image)
+            setName(res.data.name)
         })
         .catch(err => console.log(err))
     }
@@ -28,12 +29,12 @@ let searchResults = () =>{
     return(
         <div style={{fontFamily: 'Roboto'}}>
             <Navbar />
-            <div>
-                <img src="/products/food.png" className="img-fluid mt-n3 w-100"/>
+            <div className="mt-n3" style={{height: "450px",width:"100%",overflow:"hidden"}}>
+                <img src={cvr} className="img-fluid w-100" style={{marginTop:"-15rem"}}/>
             </div>
-            <div className="mt-5 mr-5 ml-5" >
+            <div className="mt-5 mr-5 ml-5" style={{paddingBottom:"160px"}} >
                 <div className="d-flex">
-                    <div className="text-left h1 font-weight-normal">{top}</div>
+                    <div className="text-left h1 font-weight-normal">{name}</div>
                     <div className="ml-auto">
                         <div className=" h2">Search Bar</div>
                     </div>
@@ -62,7 +63,7 @@ let searchResults = () =>{
                     })
                 }
             </div>
-            <div className="navbar navbar-default navbar-static-bottom navbar-fixed-bottom w-100">
+            <div className="footer">
                 <Footer />
             </div>
         </div>

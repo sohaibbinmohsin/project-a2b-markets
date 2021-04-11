628cddcfc78a5be1cb71f01ad653e6cffa63e74d
aimen-abid <74820207+aimen-abid@users.noreply.github.com>
2021-04-11 15:13:31 +0500

    Update App.js

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,5 +1,14 @@
 // import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import {  GlobalStyle } from './GlobalStyle';
+import Footer from './Components/footer.js';
+import Navbaar from './Components/indexPage/Navbar.js';
+import Markets from './Components/indexPage/Markets';
+import Categories from './Components/indexPage/Categories';
+import CarouselContainer from './Components/indexPage/carouselContainer.js';
+
 
 function App() {
   return (
@@ -9,6 +18,19 @@ function App() {
         <br />
         <a href="/location">Location Component</a>
       </div>
+    <Router>
+      <Navbaar />
+      <GlobalStyle />
+      <div className="searchBox">
+        <input className="textBox" type="text" placeholder="Enter your address"></input>
+          {/* <i className="locationIcon" class="fa fa-location" ></i>  */}
+        <button className="searchBtn" >FIND SHOPS NEAR YOU</button>
+      </div>
+      <CarouselContainer />
+      <Markets />
+      <Categories />
+              {/* <Footer /> */}
+    </Router>
     </div>
   );
 }

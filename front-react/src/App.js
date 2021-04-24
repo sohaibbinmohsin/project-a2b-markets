60e56cc8bc7a7dc5745be7e5fe42ef5c1372a529
mehnoormaqsood <77913699+mehnoormaqsood@users.noreply.github.com>
2021-04-24 15:34:07 +0500

    Update App.js

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import React from 'react';
+import {React, useEffect} from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 // import {  GlobalStyle } from './GlobalStyle';
@@ -8,9 +8,18 @@ import Navbaar from './Components/Navbar.js';
 import Markets from './Components/Markets.js';
 import Categories from './Components/Categories.js';
 import CarouselContainer from './Components/carouselContainer.js';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux'
+import { getCategories } from '././actions/markets';
 
 
 function App() {
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    dispatch(getCategories())
+},[dispatch])
+
   return (
     <div>
     <Router>

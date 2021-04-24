b5133d7923e4216faae15380d0c0ee928f38371c
mehnoormaqsood <77913699+mehnoormaqsood@users.noreply.github.com>
2021-04-24 15:57:26 +0500

    Update shoppingcart.model.js

--- models/shoppingcart.model.js
+++ models/shoppingcart.model.js
@@ -4,11 +4,15 @@ const Schema = mongoose.Schema;
 
 
 const shoppingCartSchema = new Schema({
-    products: [{
-        type: Schema.ObjectId,
-        ref: 'product',
+    products: {
+        type: Array,
         default: []
-    }]
+    },
+    total_price: {type: Number, default :0},
+    vendor_name: {type: String, default: ""},
+    vendor_id : {type: String, default: ""},
+    delivery_charges: {type: String, default:""},
+
 })
 
 const shoppingCart = mongoose.model('shoppingCart', shoppingCartSchema)

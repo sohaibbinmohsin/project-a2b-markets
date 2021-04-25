050807e2a25c9aed6a2fd6de3815af06a5bf73a0
mehnoormaqsood <77913699+mehnoormaqsood@users.noreply.github.com>
2021-04-25 13:36:53 +0500

    Update orders.model.js

--- models/orders.model.js
+++ models/orders.model.js
@@ -5,12 +5,18 @@ const MyObjectId = mongoose.Types.ObjectId
 let orderSchema =  Schema({
     customer: { type: MyObjectId, ref: 'customer' },
     vendor: { type: MyObjectId, ref: 'vendor' },
+    vendor_name : {type: String},
+    products: {
+        type: Array,
+        default: []
+    },
     total_amount : Number,
-    order_note : String,
-    discount_voucher : String,
-    order_status : String,
+    order_note : {type: String, default: ""},
+    discount_voucher : {type: String, default : ""},
+    delivery_charges : {type: String, },
     date_ordered : Date,
-    date_delivered : Date
+    date_delivered : Date,
+    isConfirmed : Boolean
 })
 
 const order = mongoose.model('order', orderSchema)
